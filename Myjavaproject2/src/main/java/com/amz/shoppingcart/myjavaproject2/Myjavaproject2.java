bbd6481d909fc05fc9def3ea30191f91b7b48f27
kthea1 <kthea1@gmail.com>
2024-09-15 22:57:22 -0500

    upload the first project

---
+++ Myjavaproject2/src/main/java/com/amz/shoppingcart/myjavaproject2/Myjavaproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amz.shoppingcart.myjavaproject2;
+
+/**
+ *
+ * @author kthea
+ */
+public class Myjavaproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
